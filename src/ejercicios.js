fad563930fa1014bb9c92eedae07574840ecb9ab
PedritoJr <pedro.cardena@tecdesoftware.edu.mx>
2025-11-18 09:44:51 -0600

    Ejercicio 1.3: Obtener un canal específico de color resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -167,31 +167,32 @@ function matrizAImagen(matriz, rutaSalida) {
  * // Ahora será {r:200, g:200, b:200, a:255} (gris)
  */
 function obtenerCanal(matriz, canal) {
-  // TODO: Implementar extracción de canal
+// TODO: Implementar extracción de canal
   
   // 1. Validar parámetros
-  // if (!['r', 'g', 'b'].includes(canal)) {
-  //   throw new Error("El canal debe ser 'r', 'g', o 'b'");
-  // }
+   if (!['r', 'g', 'b'].includes(canal)) {
+     throw new Error("El canal debe ser 'r', 'g', o 'b'");
+   }
   
   // 2. Crear matriz resultado
-  // const resultado = copiarMatriz(matriz);
+   const resultado = copiarMatriz(matriz);
   
   // 3. Para cada pixel, usar solo el valor del canal seleccionado
-  // for (let i = 0; i < resultado.length; i++) {
-  //   for (let j = 0; j < resultado[i].length; j++) {
-  //     const valor = matriz[i][j][canal];
-  //     resultado[i][j] = {
-  //       r: valor,
-  //       g: valor,
-  //       b: valor,
-  //       a: matriz[i][j].a
-  //     };
-  //   }
-  // }
+   for (let i = 0; i < resultado.length; i++) {
+     for (let j = 0; j < resultado[i].length; j++) {
+       const valor = matriz[i][j][canal];
+       resultado[i][j] = {
+         r: valor,
+         g: valor,
+         b: valor,
+         a: matriz[i][j].a
+       };
+     }
+   }
   
-  return []; // REEMPLAZAR CON TU CÓDIGO
+  return resultado; // REEMPLAZAR CON TU CÓDIGO
 }
+
 
 /**
  * Ejercicio 1.4: Obtener dimensiones de una imagen (5 puntos)
